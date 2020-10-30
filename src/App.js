f12e8dfd47fda3b2b184b9bb77675e85f3446b68
parthpatel1004 <parthpatel1004@gmail.com>
2020-10-30 18:41:45 +0530

    useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react'
 
+export default function App (){
+  const [count, setCount] = useState(0);
+  const [countDown, setCountDown] = useState(50);
 
-export default function App() {
-  const [name, setName] = useState('Parth');
-  const [roll, setRoll] = useState(133);
-  // const name = nameStateVariable[0];
-  // const setName = nameStateVariable[1];
-
-  const handleClick = () => {
-    setName('parth new state');
-    setRoll(101);
+  const handleIncrement = () => {
+    setCount(count + 1);
   };
 
+  const handleDecrement = () => {
+    setCountDown(countDown - 1);
+  };
+
+  useEffect(() => {
+    console.log('useEffect called')
+  }, [count])
+//passing array args as optional. here only count uses the useEffect as it is passing to the array
+
+
+
   return <React.Fragment>
-  <h1> {name} and roll no is {roll}</h1>
-  <button onClick={handleClick}> Change the state</button>
+    <h1>Count Up : {count}</h1>
+    <button type='button' onClick={handleIncrement}> Increment </button>
+    
+    <h1>Count down : {countDown}</h1>
+    <button type='button' onClick={handleDecrement}> Decrement </button>
   </React.Fragment>
+
 }
