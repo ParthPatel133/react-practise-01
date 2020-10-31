2059f898a2022e2c7b18294e78079e1b9864cab5
parthpatel1004 <parthpatel1004@gmail.com>
2020-10-31 18:58:34 +0530

    inline If else | Ternary Condition

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ export default class App extends Component {
   };
 
   clickLogin = () => {
-    this.setState({isLoggedIn : true})
+    this.setState({ isLoggedIn: true })
   };
 
   clickLogout = () => {
-    this.setState({isLoggedIn : false})
+    this.setState({ isLoggedIn: false })
   };
 
   render() {
     const isLoggedIn = this.state.isLoggedIn;
-    if (isLoggedIn) {
-      return <User clickData = {this.clickLogout} />;
-    } else {
-      return <Guest clickData = {this.clickLogin} />;
-    }
+    return (
+      <div>
+        {isLoggedIn ? <User clickData={this.clickLogout} />
+                    : <Guest clickData={this.clickLogin} />
+        }
+      </div>
+    )
   }
 }
