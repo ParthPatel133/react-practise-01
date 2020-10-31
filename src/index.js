0a7ac27dc670af70d10d1e508b8489e4a8f29123
parthpatel1004 <parthpatel1004@gmail.com>
2020-10-31 18:27:47 +0530

    inline If with logical &&

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App consumer={true} />, document.getElementById('root'));
+ReactDOM.render(<App primeMember={true} />, document.getElementById('root'));
