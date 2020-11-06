88777d117589762244bf58a2d4a279f992e14439
parthpatel1004 <parthpatel1004@gmail.com>
2020-11-06 11:47:47 +0530

    css module

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-//external css styling
+//css module style in react
+//this will create local scope for styling component after importing
 
 import React, { Component } from 'react'
 import './App.css'
+import Styles from './App.module.css'
 
 export default class App extends Component {
 
   render() {
     return (
       <React.Fragment>
-        <h1 className='txt'>Hello external CSS</h1>
+        <h1 className={Styles.txt}>Hello external CSS</h1>
       </React.Fragment>
     )
   }
