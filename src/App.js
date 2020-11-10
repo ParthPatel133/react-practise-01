619bac7a83026be047b13f48c62d7d2f53c8b0df
parthpatel1004 <parthpatel1004@gmail.com>
2020-11-10 23:15:35 +0530

    controlled element in forms

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-//css module style in react
-//this will create local scope for styling component after importing
-
+//---------CONTROLLED COMPONENT IN REACT FORMS------------
 import React, { Component } from 'react'
-import './App.css'
-import Styles from './App.module.css'
 
 export default class App extends Component {
+  state = {
+    value: 'input value',
+    valueTextarea: 'this is textarea'
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value.toUpperCase().substr(0, 10) })
+  }
+
+  handleTextarea = (e) => {
+    this.setState({ valueTextarea: e.target.value.substr(0, 25) })
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <h1 className={Styles.txt}>Hello external CSS</h1>
-      </React.Fragment>
+      <div>
+        <form>
+          <h2>controlled by react</h2>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <textarea
+            value={this.state.valueTextarea}
+            onChange={this.handleTextarea}
+          />
+        </form>
+      </div>
     )
   }
 }
+
 
